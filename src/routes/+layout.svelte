dc2b16e8646d618eabece6903618306ade2924b2
mattyydagz <mnjdagostino@gmail.com>
2023-07-31 13:00:56 -0400

    iconify added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,8 @@
     import '@skeletonlabs/skeleton/styles/skeleton.css';
     import { TabAnchor, TabGroup } from "@skeletonlabs/skeleton";
     
+    import 'iconify-icon'
+
     import { page } from "$app/stores";
 </script>
 
@@ -19,15 +21,21 @@
         class="bg-surface-100-800-token w-full"
     >
         <TabAnchor href="/" selected={$page.url.pathname === '/'}>
-            <svelte:fragment slot="lead">(icon)</svelte:fragment>
+            <svelte:fragment slot="lead">
+                <iconify-icon icon="mdi:home" style="font-size: 2rem;"></iconify-icon>
+            </svelte:fragment>
             <span>Home</span>
         </TabAnchor>
         <TabAnchor href="/teams" selected={$page.url.pathname === '/teams'}>
-            <svelte:fragment slot="lead">(icon)</svelte:fragment>
+            <svelte:fragment slot="lead">
+                <iconify-icon icon="mdi:account-group" style="font-size: 2rem;"></iconify-icon>
+            </svelte:fragment>
             <span>Teams</span>
         </TabAnchor>
         <TabAnchor href="/leaderboards" selected={$page.url.pathname === '/leaderboards'}>
-            <svelte:fragment slot="lead">(icon)</svelte:fragment>
+            <svelte:fragment slot="lead">
+                <iconify-icon icon="mdi:trophy" style="font-size: 2rem;"></iconify-icon>
+            </svelte:fragment>
             <span>Leaderboards</span>
         </TabAnchor>
     </TabGroup>
