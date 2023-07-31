d50e2bbeeafaf3d767483a7173b2fbda16f47324
mattyydagz <mnjdagostino@gmail.com>
2023-07-31 12:33:24 -0400

    Owners table and nav added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,37 @@
 <script lang="ts">
     import "../app.css";
+    
     import '@skeletonlabs/skeleton/themes/theme-rocket.css';
     import '@skeletonlabs/skeleton/styles/skeleton.css';
+    import { TabAnchor, TabGroup } from "@skeletonlabs/skeleton";
+    
+    import { page } from "$app/stores";
 </script>
 
 <div class="min-h-screen flex flex-col">
-    <slot />
+    <TabGroup 
+        justify="justify-end"
+        active="variant-filled-primary"
+        hover="hover:variant-soft-primary"
+        flex="flex-1 lg:flex-none"
+        rounded=""
+        border=""
+        class="bg-surface-100-800-token w-full"
+    >
+        <TabAnchor href="/" selected={$page.url.pathname === '/'}>
+            <svelte:fragment slot="lead">(icon)</svelte:fragment>
+            <span>Home</span>
+        </TabAnchor>
+        <TabAnchor href="/teams" selected={$page.url.pathname === '/teams'}>
+            <svelte:fragment slot="lead">(icon)</svelte:fragment>
+            <span>Teams</span>
+        </TabAnchor>
+        <TabAnchor href="/leaderboards" selected={$page.url.pathname === '/leaderboards'}>
+            <svelte:fragment slot="lead">(icon)</svelte:fragment>
+            <span>Leaderboards</span>
+        </TabAnchor>
+    </TabGroup>
+    <div>
+        <slot />
+    </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,16 +5,13 @@
     import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
     import type { TableSource } from '@skeletonlabs/skeleton';
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-    const sourceData = [
-        { position: 1, owner: "Matthew D'Agostino"},
-    ];
+    // default values
+    let sourceData: any[] = [];
 
-    const ownersTable: TableSource = {
-	    head: ['Owners'],
-	    body: tableMapperValues(sourceData, ['owner']),
+    let ownersTable: TableSource = {
+	    head: [],
+	    body: tableMapperValues(sourceData, []),
     };
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
 
     async function getOwnersName() {
         try {
@@ -34,16 +31,13 @@
 
     getOwnersName()
         .then((owners) => {
-            let arr: { position: number; owner: DocumentData; }[] = [];
             owners.forEach((owner, i) =>{
-                arr.push({ position: i + 1, owner: owner},)
+                sourceData.push({ position: i + 1, owner: owner},)
             }) 
-            console.log(arr);
 
-            //TODO make html table source = to below
-            const ownersTable: TableSource = {
-                head: ['Owners'],
-                body: tableMapperValues(arr, ['owner']),
+            ownersTable = {
+	            head: ['Owners'],
+	            body: tableMapperValues(sourceData, ['owner']),
             };
         })
         .catch((error) => {
@@ -61,10 +55,10 @@
     
 </script>
 
-<div class="grid place-content-center h-80">
+<div class="grid place-content-center">
     <div class="flex flex-col p-4 gap-4 text-center">
         <h1 class="text-6xl font-bold underline pb-4">Welcome to Boot Scoot n Juke!</h1>
         <h2 class="text-3xl">It's the the fantasy football league that all fantasy football leagues want to be.</h2>
     </div>
-    <Table class="w-[400px]" source={ownersTable} />
+    <Table class="w-full" source={ownersTable} />
 </div>
