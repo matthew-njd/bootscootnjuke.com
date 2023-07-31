fb86a6cffb1575c27c3038f84022a8bc05726cda
mattyydagz <mnjdagostino@gmail.com>
2023-07-31 15:50:50 -0400

    Added team folder and basic page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
     import '@skeletonlabs/skeleton/styles/skeleton.css';
 
     import { AppShell } from '@skeletonlabs/skeleton';
-    import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+    import { AppRail, AppRailAnchor } from '@skeletonlabs/skeleton';
 
     import { page } from '$app/stores';
     
@@ -20,20 +20,18 @@
                 <AppRailAnchor href="/" >(Boot Scoot n Juke)</AppRailAnchor>
             </svelte:fragment>
             <AppRailAnchor href="/" selected={$page.url.pathname === '/'}>
-                <iconify-icon icon="mdi:home" style="font-size: 2rem;"></iconify-icon>
+                <iconify-icon icon="mdi:home" style="font-size: 2rem;" title="Home"></iconify-icon>
             </AppRailAnchor>
 	        <AppRailAnchor href="/teams" selected={$page.url.pathname === '/teams'}>
-                <iconify-icon icon="mdi:account-group" style="font-size: 2rem;"></iconify-icon>
+                <iconify-icon icon="mdi:account-group" style="font-size: 2rem;" title="Teams"></iconify-icon>
             </AppRailAnchor>
 	        <AppRailAnchor href="/leaderboards" selected={$page.url.pathname === '/leaderboards'}>
-                <iconify-icon icon="mdi:trophy" style="font-size: 2rem;"></iconify-icon>
+                <iconify-icon icon="mdi:trophy" style="font-size: 2rem;" title="Leaderboards"></iconify-icon>
             </AppRailAnchor>
         </AppRail>
     </svelte:fragment>
-	<!-- (sidebarRight) -->
-	<!-- (pageHeader) -->
-	<!-- Router Slot -->
+    
 	<slot />
-	<!-- ---- / ---- -->
+
 	<svelte:fragment slot="footer">Footer</svelte:fragment>
 </AppShell>
---
+++ src/routes/teams/[team]/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+
+</script>
+
+<h1>Team 1</h1>
