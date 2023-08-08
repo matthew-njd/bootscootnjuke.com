c52810fcc1f70d8c3b65b0ab7972b4f72437e974
mattyydagz <mnjdagostino@gmail.com>
2023-08-08 15:02:31 -0400

    image updates

--- static/images/index.js
+++ static/images/index.js
@@ -1,5 +1,3 @@
-import gm_logo from "./gord_mccusker_logo.png";
-import md_logo from "./matthew_dagostino_logo.png";
-import sr_logo from "./shayne_rover_logo.jpg";
+import nfl from "./league_logo.jpg";
 
-export { gm_logo, md_logo, sr_logo };
+export { nfl };
