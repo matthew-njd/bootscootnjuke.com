0a6404ab89ccb2bd51351ba5b513f8e89da59d7e
Matthew D'Agostino <mnjdagostino@gmail.com>
2023-09-01 10:54:45 -0400

    drafts page updates

--- src/lib/components/DraftCard.svelte
+++ src/lib/components/DraftCard.svelte
@@ -1,8 +1,8 @@
-<div class="card card-hover m-1">
-    <header class="flex justify-center card-header text-xl text-center">
+<div class="flex flex-col card card-hover m-1 w-[175px] h-[275px]">
+    <header class="card-header text-xl text-center">
         <slot name="player"/>
     </header>
-    <section class="text-xl text-center pt-6 pb-2 text-gray-400">
+    <section class="text-lg text-center text-gray-400">
         <slot name="team" />
     </section>
 </div>
--- src/routes/drafts/+page.svelte
+++ src/routes/drafts/+page.svelte
@@ -1,10 +1,27 @@
 <script lang="ts">
     import DraftCard from "$lib/components/DraftCard.svelte";
-import type { Drafts } from "$lib/types"
+    import type { Drafts } from "$lib/types"
+    import { Tab, TabGroup } from "@skeletonlabs/skeleton";
 
     export let data:Drafts;
 
     const firstRound = data.draft.slice(0,10);
+    const secondRound = data.draft.slice(10,20);
+    const thirdRound = data.draft.slice(20,30);
+    const fourthRound = data.draft.slice(30,40);
+    const fifthRound = data.draft.slice(40,50);
+    const sixthRound = data.draft.slice(50,60);
+    const seventhRound = data.draft.slice(60,70);
+    const eigthRound = data.draft.slice(70,80);
+    const ninthRound = data.draft.slice(80,90);
+    const tenthRound = data.draft.slice(90,100);
+    const eleventhRound = data.draft.slice(100,110);
+    const twelfthRound = data.draft.slice(110,120);
+    const thirteenthRound = data.draft.slice(120,130);
+    const fourteenthRound = data.draft.slice(130,140);
+    const fifteenthRound = data.draft.slice(140,150);
+
+    let tabSet: number = 0;
 </script>
 
 <h1 class="flex justify-center align-baseline text-6xl pt-8 pb-16 gap-3">
@@ -12,19 +29,302 @@ import type { Drafts } from "$lib/types"
     <iconify-icon icon="ri:draft-fill" style="font-size: 4rem;" title="Drafts" />
 </h1>
 
-<h2 class="text-4xl text-center p-4">
-    Round 1
-</h2>
-<div class="flex">
-    {#each firstRound as { player, playerHeadshotUrl, team }}
-        <DraftCard>
-            <span slot="player">
-                <img src={playerHeadshotUrl} alt="Player Headshot" class="w-[150px] rounded-full"/>
-                {player}
-            </span>
-            <span slot="team">
-                {team}
-            </span>
-        </DraftCard>
-    {/each}
-</div>
+<TabGroup justify="justify-center">
+    <Tab bind:group={tabSet} name="tab1" value={0}>
+		2023 Draft
+	</Tab>
+	<Tab bind:group={tabSet} name="tab2" value={1}>
+        2022 Draft
+    </Tab>
+	<!-- Tab Panels --->
+	<svelte:fragment slot="panel">
+        {#if tabSet === 0}
+        <section>
+            <h2 class="text-4xl text-center p-4">
+                Round 1
+            </h2>
+            <div class="flex justify-center flex-wrap">
+                {#each firstRound as { player, playerHeadshotUrl, team }}
+                    <DraftCard>
+                        <span slot="player">
+                            <img src={playerHeadshotUrl} alt="Player Headshot" class="rounded-full"/>
+                            {player}
+                        </span>
+                        <span slot="team">
+                            {team}
+                        </span>
+                    </DraftCard>
+                {/each}
+            </div>
+        </section>
+        
+        <section>
+            <h2 class="text-4xl text-center p-4">
+                Round 2
+            </h2>
+            <div class="flex justify-center flex-wrap">
+                {#each secondRound as { player, playerHeadshotUrl, team }}
+                    <DraftCard>
+                        <span slot="player">
+                            <img src={playerHeadshotUrl} alt="Player Headshot" class="object-cover rounded-full"/>
+                            {player}
+                        </span>
+                        <span slot="team">
+                            {team}
+                        </span>
+                    </DraftCard>
+                {/each}
+            </div>
+        </section>
+        
+        <section>
+            <h2 class="text-4xl text-center p-4">
+                Round 3
+            </h2>
+            <div class="flex justify-center flex-wrap">
+                {#each thirdRound as { player, playerHeadshotUrl, team }}
+                    <DraftCard>
+                        <span slot="player">
+                            <img src={playerHeadshotUrl} alt="Player Headshot" class="object-cover rounded-full"/>
+                            {player}
+                        </span>
+                        <span slot="team">
+                            {team}
+                        </span>
+                    </DraftCard>
+                {/each}
+            </div>
+        </section>
+        
+        <section>
+            <h2 class="text-4xl text-center p-4">
+                Round 4
+            </h2>
+            <div class="flex justify-center flex-wrap">
+                {#each fourthRound as { player, playerHeadshotUrl, team }}
+                    <DraftCard>
+                        <span slot="player">
+                            <img src={playerHeadshotUrl} alt="Player Headshot" class="object-cover rounded-full"/>
+                            {player}
+                        </span>
+                        <span slot="team">
+                            {team}
+                        </span>
+                    </DraftCard>
+                {/each}
+            </div>
+        </section>
+        
+        <section>
+            <h2 class="text-4xl text-center p-4">
+                Round 5
+            </h2>
+            <div class="flex justify-center flex-wrap">
+                {#each fifthRound as { player, playerHeadshotUrl, team }}
+                    <DraftCard>
+                        <span slot="player">
+                            <img src={playerHeadshotUrl} alt="Player Headshot" class="object-cover rounded-full"/>
+                            {player}
+                        </span>
+                        <span slot="team">
+                            {team}
+                        </span>
+                    </DraftCard>
+                {/each}
+            </div>
+        </section>
+        
+        <section>
+            <h2 class="text-4xl text-center p-4">
+                Round 6
+            </h2>
+            <div class="flex justify-center flex-wrap">
+                {#each sixthRound as { player, playerHeadshotUrl, team }}
+                    <DraftCard>
+                        <span slot="player">
+                            <img src={playerHeadshotUrl} alt="Player Headshot" class="object-cover rounded-full"/>
+                            {player}
+                        </span>
+                        <span slot="team">
+                            {team}
+                        </span>
+                    </DraftCard>
+                {/each}
+            </div>
+        </section>
+        
+        <section>
+            <h2 class="text-4xl text-center p-4">
+                Round 7
+            </h2>
+            <div class="flex justify-center flex-wrap">
+                {#each seventhRound as { player, playerHeadshotUrl, team }}
+                    <DraftCard>
+                        <span slot="player">
+                            <img src={playerHeadshotUrl} alt="Player Headshot" class="object-cover rounded-full"/>
+                            {player}
+                        </span>
+                        <span slot="team">
+                            {team}
+                        </span>
+                    </DraftCard>
+                {/each}
+            </div>
+        </section>
+        
+        <section>
+            <h2 class="text-4xl text-center p-4">
+                Round 8
+            </h2>
+            <div class="flex justify-center flex-wrap">
+                {#each eigthRound as { player, playerHeadshotUrl, team }}
+                    <DraftCard>
+                        <span slot="player">
+                            <img src={playerHeadshotUrl} alt="Player Headshot" class="object-cover rounded-full"/>
+                            {player}
+                        </span>
+                        <span slot="team">
+                            {team}
+                        </span>
+                    </DraftCard>
+                {/each}
+            </div>
+        </section>
+        
+        <section>
+            <h2 class="text-4xl text-center p-4">
+                Round 9
+            </h2>
+            <div class="flex justify-center flex-wrap">
+                {#each ninthRound as { player, playerHeadshotUrl, team }}
+                    <DraftCard>
+                        <span slot="player">
+                            <img src={playerHeadshotUrl} alt="Player Headshot" class="object-cover rounded-full"/>
+                            {player}
+                        </span>
+                        <span slot="team">
+                            {team}
+                        </span>
+                    </DraftCard>
+                {/each}
+            </div>
+        </section>
+        
+        <section>
+            <h2 class="text-4xl text-center p-4">
+                Round 10
+            </h2>
+            <div class="flex justify-center flex-wrap">
+                {#each tenthRound as { player, playerHeadshotUrl, team }}
+                    <DraftCard>
+                        <span slot="player">
+                            <img src={playerHeadshotUrl} alt="Player Headshot" class="object-cover rounded-full"/>
+                            {player}
+                        </span>
+                        <span slot="team">
+                            {team}
+                        </span>
+                    </DraftCard>
+                {/each}
+            </div>
+        </section>
+        
+        <section>
+            <h2 class="text-4xl text-center p-4">
+                Round 11
+            </h2>
+            <div class="flex justify-center flex-wrap">
+                {#each eleventhRound as { player, playerHeadshotUrl, team }}
+                    <DraftCard>
+                        <span slot="player">
+                            <img src={playerHeadshotUrl} alt="Player Headshot" class="object-cover rounded-full"/>
+                            {player}
+                        </span>
+                        <span slot="team">
+                            {team}
+                        </span>
+                    </DraftCard>
+                {/each}
+            </div>
+        </section>
+        
+        <section>
+            <h2 class="text-4xl text-center p-4">
+                Round 12
+            </h2>
+            <div class="flex justify-center flex-wrap">
+                {#each twelfthRound as { player, playerHeadshotUrl, team }}
+                    <DraftCard>
+                        <span slot="player">
+                            <img src={playerHeadshotUrl} alt="Player Headshot" class="object-cover rounded-full"/>
+                            {player}
+                        </span>
+                        <span slot="team">
+                            {team}
+                        </span>
+                    </DraftCard>
+                {/each}
+            </div>
+        </section>
+        
+        <section>
+            <h2 class="text-4xl text-center p-4">
+                Round 13
+            </h2>
+            <div class="flex justify-center flex-wrap">
+                {#each thirteenthRound as { player, playerHeadshotUrl, team }}
+                    <DraftCard>
+                        <span slot="player">
+                            <img src={playerHeadshotUrl} alt="Player Headshot" class="object-cover rounded-full"/>
+                            {player}
+                        </span>
+                        <span slot="team">
+                            {team}
+                        </span>
+                    </DraftCard>
+                {/each}
+            </div>
+        </section>
+        
+        <section>
+            <h2 class="text-4xl text-center p-4">
+                Round 14
+            </h2>
+            <div class="flex justify-center flex-wrap">
+                {#each fourteenthRound as { player, playerHeadshotUrl, team }}
+                    <DraftCard>
+                        <span slot="player">
+                            <img src={playerHeadshotUrl} alt="Player Headshot" class="object-cover rounded-full"/>
+                            {player}
+                        </span>
+                        <span slot="team">
+                            {team}
+                        </span>
+                    </DraftCard>
+                {/each}
+            </div>
+        </section>
+        
+        <section>
+            <h2 class="text-4xl text-center p-4">
+                Round 15
+            </h2>
+            <div class="flex justify-center flex-wrap">
+                {#each fifteenthRound as { player, playerHeadshotUrl, team }}
+                    <DraftCard>
+                        <span slot="player">
+                            <img src={playerHeadshotUrl} alt="Player Headshot" class="object-cover rounded-full"/>
+                            {player}
+                        </span>
+                        <span slot="team">
+                            {team}
+                        </span>
+                    </DraftCard>
+                {/each}
+            </div>
+        </section>
+		{/if}
+        
+	</svelte:fragment>
+</TabGroup>
+
