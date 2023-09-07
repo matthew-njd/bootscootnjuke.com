8b5b971a2da99fbf002d8799ce8def2e5e3a8c5a
Matthew D'Agostino <mnjdagostino@gmail.com>
2023-09-07 11:28:55 -0400

    home page updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <div class="flex flex-col m-auto max-w-[1080px] gap-4">
-    <h1 class="text-center text-6xl font-bold pb-4">Welcome to the site for the fantasy football league, Boot Scoot n Juke!</h1>
+    <h1 class="text-center text-6xl font-bold pb-4">Greetings and Welcome to <u>Boot Scoot n Juke</u>: The Premier Fantasy Football League.</h1>
     <h2 class="text-center text-3xl">
-        A place to check out the different owners and their team's jaw-dropping stats! Unearth the titans who've dominated the leaderboards, and get set for a heart-pounding journey through draft history!
+        Here you can check out the different owners and their team's jaw-dropping stats! Unearth the titans who've dominated the leaderboards, and get set for a heart-pounding journey through the draft history!
     </h2>
 </div>
