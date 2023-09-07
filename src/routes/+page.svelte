18191275ea12914d67bcf1ec3c369d498f2afc20
Matthew D'Agostino <mnjdagostino@gmail.com>
2023-09-07 11:22:44 -0400

    home screen updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,9 @@
     
 </script>
 
-<div class="flex flex-col pt-8 gap-4 text-center">
-    <h1 class="text-6xl font-bold underline pb-4">Welcome to Boot Scoot n Juke!</h1>
-    <h2 class="text-3xl">
-        Drafting dreams and juking schemes.
+<div class="flex flex-col m-auto max-w-[1080px] gap-4">
+    <h1 class="text-center text-6xl font-bold pb-4">Welcome to the site for the fantasy football league, Boot Scoot n Juke!</h1>
+    <h2 class="text-center text-3xl">
+        A place to check out the different owners and their team's jaw-dropping stats! Unearth the titans who've dominated the leaderboards, and get set for a heart-pounding journey through draft history!
     </h2>
 </div>
