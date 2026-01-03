95a5d9782d32b1fb6d48430db1b2a4dba0a36350
Matthew D'Agostino <mnjdagostino@gmail.com>
2026-01-03 14:03:01 -0500

    Added option button to card component. Add button to Owners page

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,25 +1,30 @@
 interface CardProps {
   title: React.ReactNode;
   body: React.ReactNode;
+  footer?: React.ReactNode;
   className?: string;
   cardBodyClassName?: string;
   titleClassName?: string;
   bodyClassName?: string;
+  footerClassName?: string;
 }
 
 export default function Card({
   title,
   body,
+  footer,
   className = "",
   cardBodyClassName = "card-body",
   titleClassName = "card-title",
-  bodyClassName = "",
+  bodyClassName = "text-left",
+  footerClassName = "card-actions justify-end",
 }: CardProps) {
   return (
     <div className={className}>
       <div className={cardBodyClassName}>
         <h2 className={titleClassName}>{title}</h2>
         <p className={bodyClassName}>{body}</p>
+        {footer && <div className={footerClassName}>{footer}</div>}
       </div>
     </div>
   );
--- src/pages/Owners.tsx
+++ src/pages/Owners.tsx
@@ -38,8 +38,21 @@ export default function Owners() {
   }
 
   return (
-    <div>
-      <h1 className="text-4xl mb-6">Owners</h1>
+    <div className="flex flex-col items-center">
+      <div className="flex gap-2">
+        <h1 className="text-6xl mb-6">Owners</h1>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="4em"
+          height="4em"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M12 5.5A3.5 3.5 0 0 1 15.5 9a3.5 3.5 0 0 1-3.5 3.5A3.5 3.5 0 0 1 8.5 9A3.5 3.5 0 0 1 12 5.5M5 8c.56 0 1.08.15 1.53.42c-.15 1.43.27 2.85 1.13 3.96C7.16 13.34 6.16 14 5 14a3 3 0 0 1-3-3a3 3 0 0 1 3-3m14 0a3 3 0 0 1 3 3a3 3 0 0 1-3 3c-1.16 0-2.16-.66-2.66-1.62a5.54 5.54 0 0 0 1.13-3.96c.45-.27.97-.42 1.53-.42M5.5 18.25c0-2.07 2.91-3.75 6.5-3.75s6.5 1.68 6.5 3.75V20h-13zM0 20v-1.5c0-1.39 1.89-2.56 4.45-2.9c-.59.68-.95 1.62-.95 2.65V20zm24 0h-3.5v-1.75c0-1.03-.36-1.97-.95-2.65c2.56.34 4.45 1.51 4.45 2.9z"
+          ></path>
+        </svg>
+      </div>
       <div className="flex flex-col gap-4">
         {owners
           .filter((owner) => owner.active)
@@ -59,6 +72,24 @@ export default function Owners() {
                 </>
               }
               body={owner.bio || "No bio available"}
+              footer={
+                <>
+                  <button type="button" className="btn btn-accent">
+                    Stats
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="m14 18l-1.4-1.45L16.15 13H4v-2h12.15L12.6 7.45L14 6l6 6z"
+                      />
+                    </svg>
+                  </button>
+                </>
+              }
               className="card bg-primary text-primary-content w-full"
               titleClassName="card-title text-xl font-bold"
               bodyClassName="text-left text-lg"
