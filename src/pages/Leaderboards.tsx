c2f579b8ab6a7ba5e747d7062abeb9025bca6eb0
Matthew Linux <mnjdagostino@gmail.com>
2026-01-03 17:45:17 -0500

    Update titles on each page.

--- src/pages/Leaderboards.tsx
+++ src/pages/Leaderboards.tsx
@@ -1,5 +1,22 @@
 function Leaderboards() {
-  return <h1>Leaderboards</h1>;
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex gap-2">
+        <h1 className="text-6xl mb-6">Leaderboards</h1>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="4em"
+          height="4em"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M2 21V9h5.5v12zm7.25 0V3h5.5v18zm7.25 0V11H22v10z"
+          />
+        </svg>
+      </div>
+    </div>
+  );
 }
 
 export default Leaderboards;
