256094eba9321cf96e6acf755770d3e11c475cf5
Matthew D'Agostino <mnjdagostino@gmail.com>
2026-01-03 20:10:54 -0500

    Style updates.

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -4,14 +4,18 @@ function Nav() {
   return (
     <div className="navbar justify-between mb-12 text-primary">
       <div className="w-32">
-        <Link to="/" className="hover:scale-110">
-          <img src="src/assets/images/league_logo.png" alt="League Logo" />
+        <Link to="/">
+          <img
+            src="src/assets/images/league_logo.png"
+            alt="League Logo"
+            className="hover:scale-110"
+          />
         </Link>
       </div>
-      <div className="flex gap-6">
+      <div className="flex gap-4">
         <Link
           to="/owners"
-          className="flex flex-col items-center gap-1 hover:opacity-70"
+          className="flex flex-col items-center gap-1 hover:opacity-70 hover:underline"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -24,12 +28,12 @@ function Nav() {
               d="M12 5.5A3.5 3.5 0 0 1 15.5 9a3.5 3.5 0 0 1-3.5 3.5A3.5 3.5 0 0 1 8.5 9A3.5 3.5 0 0 1 12 5.5M5 8c.56 0 1.08.15 1.53.42c-.15 1.43.27 2.85 1.13 3.96C7.16 13.34 6.16 14 5 14a3 3 0 0 1-3-3a3 3 0 0 1 3-3m14 0a3 3 0 0 1 3 3a3 3 0 0 1-3 3c-1.16 0-2.16-.66-2.66-1.62a5.54 5.54 0 0 0 1.13-3.96c.45-.27.97-.42 1.53-.42M5.5 18.25c0-2.07 2.91-3.75 6.5-3.75s6.5 1.68 6.5 3.75V20h-13zM0 20v-1.5c0-1.39 1.89-2.56 4.45-2.9c-.59.68-.95 1.62-.95 2.65V20zm24 0h-3.5v-1.75c0-1.03-.36-1.97-.95-2.65c2.56.34 4.45 1.51 4.45 2.9z"
             ></path>
           </svg>
-          <span className="text-sm">Owners</span>
+          <span className="text-md">Owners</span>
         </Link>
 
         <Link
           to="/leaderboards"
-          className="flex flex-col items-center gap-1 hover:opacity-70"
+          className="flex flex-col items-center gap-1 hover:opacity-70 hover:underline"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -42,12 +46,12 @@ function Nav() {
               d="M2 21V9h5.5v12zm7.25 0V3h5.5v18zm7.25 0V11H22v10z"
             />
           </svg>
-          <span className="text-sm">Leaderboards</span>
+          <span className="text-md">Leaderboards</span>
         </Link>
 
         <Link
           to="/matchups"
-          className="flex flex-col items-center gap-1 hover:opacity-70"
+          className="flex flex-col items-center gap-1 hover:opacity-70 hover:underline"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -60,12 +64,12 @@ function Nav() {
               d="m19.05 21.6l-2.925-2.9l-1.5 1.5q-.275.275-.7.275t-.7-.275q-.575-.575-.575-1.425t.575-1.425l4.225-4.225q.575-.575 1.425-.575t1.425.575q.275.275.275.7t-.275.7l-1.5 1.5l2.9 2.925q.3.3.3.7t-.3.7l-1.25 1.25q-.3.3-.7.3t-.7-.3M21.7 6.2L10.65 17.25l.125.1q.575.575.575 1.425t-.575 1.425q-.275.275-.7.275t-.7-.275l-1.5-1.5l-2.925 2.9q-.3.3-.7.3t-.7-.3L2.3 20.35q-.3-.3-.3-.7t.3-.7l2.9-2.925l-1.5-1.5q-.275-.275-.275-.7t.275-.7q.575-.575 1.425-.575t1.425.575l.1.125L17.425 2.475q.275-.275.638-.425t.762-.15H21q.425 0 .713.288T22 2.9v2.575q0 .2-.075.388T21.7 6.2M6.225 10.125l-3.65-3.65Q2.3 6.2 2.15 5.838T2 5.075V2.9q0-.425.288-.712T3 1.9h2.175q.4 0 .763.15t.637.425l3.65 3.65q.3.3.3.713t-.3.712L7.65 10.125q-.3.3-.712.3t-.713-.3"
             />
           </svg>
-          <span className="text-sm">Matchups</span>
+          <span className="text-md">Matchups</span>
         </Link>
 
         <Link
           to="/drafts"
-          className="flex flex-col items-center gap-1 hover:opacity-70"
+          className="flex flex-col items-center gap-1 hover:opacity-70 hover:underline"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -78,7 +82,7 @@ function Nav() {
               d="M8 17q.425 0 .713-.288T9 16t-.288-.712T8 15t-.712.288T7 16t.288.713T8 17m0-4q.425 0 .713-.288T9 12t-.288-.712T8 11t-.712.288T7 12t.288.713T8 13m0-4q.425 0 .713-.288T9 8t-.288-.712T8 7t-.712.288T7 8t.288.713T8 9m3 8h6v-2h-6zm0-4h6v-2h-6zm0-4h6V7h-6zM5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.587 1.413T19 21z"
             />
           </svg>
-          <span className="text-sm">Drafts</span>
+          <span className="text-md">Drafts</span>
         </Link>
       </div>
     </div>
