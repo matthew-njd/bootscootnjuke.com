a5b30a76188c712104d00fe1a9caaa1723b53369
Matthew D'Agostino <mnjdagostino@gmail.com>
2026-01-06 11:14:12 -0500

    Updated supabase to Publishable API key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,9 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "../types";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient<Database>(
+  supabaseUrl,
+  supabasePublishableKey
+);
